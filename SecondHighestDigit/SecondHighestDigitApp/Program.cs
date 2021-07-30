eeac5c90f14bb5dfe2c09573c40610f18ca39a9d
Sadedina <sadedina@live.it>
2021-07-30 20:42:52 +0100

    Working solution to problem

---
+++ SecondHighestDigit/SecondHighestDigitApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SecondHighestDigitApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = {1, 2, 3, 4, 5};
+
+            Function.Highest2Integer(numArray);           
+        }
+    }
+}
