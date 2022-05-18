628d6ed66bca5ae9e53a63b611ddf22192af889d
Samuel Adedina <samuel.adedina@asos.com>
2022-05-18 14:54:13 +0100

    Removed comments

--- CodeKata/CodeKata_10/CodeKata_14 - Assessment.cs
+++ CodeKata/CodeKata_10/CodeKata_14 - Assessment.cs
@@ -1,4 +1,30 @@
 ﻿/*
+ * Input
+    * CloCk
+    *  EXIT
+    *  CUrrENTdT
+    *  STopW_5
+    *  LastSTopwTImerS
+    * 
+    * OrGANiser
+    *  EXIT
+    *  STorelIST[]
+    *  STorelIST[Applex10, Bananax24]
+    *  STORElist[Applex10, Bananax24, Orangex2, Lemonx23]
+    *  REturnLisTS
+    *  DeletELIST_1
+    *  
+    * LiGHts
+    *  return_aUto_OFF
+    *  retuRn_auto_ON
+    *  ChanGe_AuTo_Off_0100
+    *  ChanGe_AuTo_On_2200
+    *  lIghT_STatus
+    * 
+    * Reset
+    * Exit
+    
+
     SMART HOME
     Design the workings of a smart home controller that can serve a clock, an organiser and a 
     lighting controller. 
--- CodeKata/Program.cs
+++ CodeKata/Program.cs
@@ -8,40 +8,6 @@ namespace CodeKata
         static void Main(string[] args)
         {
             CodeKata_14.SmartHome();
-
-            /* Input
-             * 
-             * CloCk
-             *  EXIT
-             *  CUrrENTdT
-             *  STopW_5
-             *  LastSTopwTImerS
-             * 
-             * OrGANiser
-             *  EXIT
-             *  STorelIST[]
-             *  STorelIST[Applex10, Bananax24]
-             *  STORElist[Applex10, Bananax24, Orangex2, Lemonx23]
-             *  REturnLisTS
-             *  DeletELIST_1
-             *  
-             * LiGHts
-             *  return_aUto_OFF
-             *  retuRn_auto_ON
-             *  ChanGe_AuTo_Off_0100
-             *  ChanGe_AuTo_On_2200
-             *  lIghT_STatus
-             * 
-             * Reset
-             * Exit
-             * 
-
-             * 
-             * 
-             * 
-             * 
-             * 
-             */
         }
     }
 }
