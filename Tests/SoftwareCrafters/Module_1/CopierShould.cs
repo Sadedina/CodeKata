b90f0a079b144b03327b7b09162130f8fc9afe3f
Saf T-Flaire <Saf T-Flaire@DESKTOP-OV37NBC>
2023-08-01 17:25:38 +0100

    Started module_1

--- Tests/SoftwareCrafters/Module_1/CopierShould.cs
+++ Tests/SoftwareCrafters/Module_1/CopierShould.cs
@@ -2,17 +2,16 @@
 
 namespace CharacterCopier.UnitTests
 {
-/*
-    - Tip:  resist the urge to immediately install testing libraries.  
-    - The exercise is to start by hand-crafting your own test doubles, to see how the various types work under the hood.
-*/
+    /*
+        - Tip:  resist the urge to immediately install testing libraries.  
+        - The exercise is to start by hand-crafting your own test doubles, to see how the various types work under the hood.
+    */
 
     public class CopierShould
     {
         [Test]
         public void DoSomething()
         {
-            Assert.Pass();
         }
     }
 }
---
+++ Tests/SoftwareCrafters/Module_1/Dummy.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+
+namespace Tests.SoftwareCrafters.Module_1;
+
+/*
+* The dummies are objects that our SUT depends on, but they are never used.
+* We don’t care about them because they are irrelevant to the test scope.
+*/
+
+public interface IDummy
+{
+    public void IrrelevantMethod();
+}
+
+public static class Dummy
+{
+    private static string output = "something";
+
+    public static string Format(IDummy dummy)
+    {
+        // "IDummy" won't interfere in the expected result.
+        dummy.IrrelevantMethod();
+
+        return output;
+    }
+}
+
+public class DummyTest
+{
+    public void Format()
+    {
+        // Notice as the parameter is irrelevant.
+        var result = Dummy.Format(null);
+
+        result.Should().Be("something");
+    }
+}
