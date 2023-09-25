dae0ce2dc43d5430ed8d57f4087342a70b3ad405
Saf T-Flaire <Saf T-Flaire@DESKTOP-OV37NBC>
2023-09-25 15:43:16 +0100

    Added CodeWars 3

---
+++ CodeWars/CodeWar_3.cs
@@ -0,0 +1,59 @@
+﻿/*
+*                                  CodeWars Number 3
+*                                  Price of Mangoes
+*           https://www.codewars.com/kata/57a77726bb9944d000000b06
+*/
+
+namespace CodeWars
+{
+    public static class CodeWar_3
+    {
+        public static int Mango(int quantity, int price)
+            => price * (quantity - quantity / 3);
+
+        public static int SumNoDuplicates(int[] arr)
+        {
+            var arrToList = new List<int>();
+
+            arrToList.AddRange(arr.ToList().Where(item => arr.Count(x => x == item) == 1));
+
+            return arrToList.Sum();
+        }
+
+        public static int SumNoDuplicates_VersionTwo(int[] arr)
+        {
+            var arrToList = arr.ToList();
+
+            foreach (var item in arr.ToList())
+            {
+                if (arr.Count(x => x == item) > 1)
+                    arrToList.RemoveAll(x => x == item);
+            }
+
+            return arrToList.Sum();
+        }
+
+        public static int SumNoDuplicates_VersionOne(int[] arr)
+        {
+            var newArr = new List<int>();
+            var discarded = new List<int>();
+
+            foreach (var item in arr)
+            {
+                if (!newArr.Contains(item))
+                {
+                    newArr.Add(item);
+                }
+                else
+                {
+                    if (!discarded.Contains(item))
+                        newArr.Add(-item);
+
+                    discarded.Add(item);
+                }
+            }
+
+            return newArr.Sum();
+        }
+    }
+}
---
+++ Tests/CodeWars/CW_3_Tests.cs
@@ -0,0 +1,68 @@
+﻿using CodeWars;
+using NUnit.Framework;
+
+namespace Tests.CodeWars;
+
+[TestFixture]
+public class CW_3_Tests
+{
+    [Test]
+    public void Mango_GivenOneMango_ReturnsThree()
+        => Assert.AreEqual(3, CodeWar_3.Mango(1, 3));
+
+    [Test]
+    public void Mango_GivenTwoMangoes_ReturnsSix()
+        => Assert.AreEqual(6, CodeWar_3.Mango(2, 3));
+
+    [Test]
+    public void Mango_GivenThreeMangoes_ReturnsSix()
+        => Assert.AreEqual(6, CodeWar_3.Mango(3, 3));
+
+    [Test]
+    public void Mango_GivenFourMangoes_ReturnsSix()
+        => Assert.AreEqual(9, CodeWar_3.Mango(4, 3));
+
+    [Test]
+    public void Mango_GivenFiveMangoes_ReturnsTwelve()
+        => Assert.AreEqual(12, CodeWar_3.Mango(5, 3));
+
+    [Test]
+    public void Mango_GivenSixMangoes_ReturnsTwelve()
+        => Assert.AreEqual(12, CodeWar_3.Mango(6, 3));
+
+    [Test]
+    public void Mango_GivenSevenMangoes_ReturnsFifteen()
+        => Assert.AreEqual(15, CodeWar_3.Mango(7, 3));
+
+    [Test]
+    public void BasicTests()
+    {
+        Assert.AreEqual(6, CodeWar_3.Mango(3, 3));
+        Assert.AreEqual(30, CodeWar_3.Mango(9, 5));
+    }
+
+    [TestCase(new int[] { 1 }, 1)]
+    [TestCase(new int[] { 1, 2, 3 }, 6)]
+    [TestCase(new int[] { 1, 2, 3, 4 }, 10)]
+    public void SumNoDuplicates_GivenAnArrayWithNoDuplicates_ReturnsSumOfArray(int[] arr, int expectedSum)
+        => Assert.AreEqual(expectedSum, CodeWar_3.SumNoDuplicates(arr));
+
+    [TestCase(new int[] { 1, 1 }, 0)]
+    [TestCase(new int[] { 1, 2, 3, 1 }, 5)]
+    [TestCase(new int[] { 1, 2, 3, 4, 2 }, 8)]
+    public void SumNoDuplicates_GivenAnArrayWithDuplicates_ReturnsSumOfArray(int[] arr, int expectedSum)
+        => Assert.AreEqual(expectedSum, CodeWar_3.SumNoDuplicates(arr));
+
+    [TestCase(new int[] { 1, 1, 1 }, 0)]
+    [TestCase(new int[] { 1, 2, 3, 1, 1 }, 5)]
+    [TestCase(new int[] { 1, 2, 3, 4, 2, 2 }, 8)]
+    public void SumNoDuplicates_GivenAnArrayWithTriples_ReturnsSumOfArray(int[] arr, int expectedSum)
+        => Assert.AreEqual(expectedSum, CodeWar_3.SumNoDuplicates(arr));
+
+    [Test]
+    public void MyTest()
+    {
+        Assert.AreEqual(5, CodeWar_3.SumNoDuplicates(new int[] { 1, 1, 2, 3 }));
+        Assert.AreEqual(3, CodeWar_3.SumNoDuplicates(new int[] { 1, 1, 2, 2, 3 }));
+    }
+}
