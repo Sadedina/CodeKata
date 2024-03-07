cf0848a3dd1a065fa84a1603f731c05a1abc5620
Samuel Adedina <samuel.adedina@asos.com>
2024-03-07 15:50:47 +0000

    Added another Kata: Code Wars 1

--- CodeWars/CodeWar_1.cs
+++ CodeWars/CodeWar_1.cs
@@ -2,12 +2,15 @@
 *                                  CodeWars Number 1
 *                                  Find the odd int
 *           https://www.codewars.com/kata/54da5a58ea159efa38000836/train/csharp
+*                                   Mumbling
+*           https://www.codewars.com/kata/5667e8f4e3f572a8f2000039
 */
 
 namespace CodeWars
 {
     public class CodeWar_1
     {
+        #region Find the odd int
         public static int find_it(int[] seq)
         {
             foreach (var item in seq.Where(item => seq.Count(x => x == item) % 2 != 0))
@@ -17,5 +20,26 @@ namespace CodeWars
 
             return -1;
         }
+        #endregion
+
+        #region Mumbling
+        public static String Accum(string s)
+        {
+            var mumble = "";
+            var wordLength = s.Length;
+
+            for (int i = 0; i < wordLength; i++)
+            {
+                mumble += s[i].ToString().ToUpper();
+
+                for (int j = 0; j < i; j++)
+                    mumble += s[i].ToString().ToLower();
+
+                mumble += "-";
+            }
+
+            return mumble.Remove(mumble.Length - 1);
+        }
+        #endregion
     }
 }
--- Tests/CodeWars/CW_1_Tests.cs
+++ Tests/CodeWars/CW_1_Tests.cs
@@ -6,6 +6,7 @@ namespace Tests.CodeWars;
 [TestFixture]
 public class CW_1_Tests
 {
+    #region Find the odd int
     [Test]
     public void Tests()
     {
@@ -31,5 +32,52 @@ public class CW_1_Tests
 
         Assert.AreEqual(-5, solution);
     }
+
+
+    #endregion
+
+    #region Mumbling
+    private static void testing(string actual, string expected)
+    {
+        Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public static void test1()
+    {
+        testing(CodeWar_1.Accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+        testing(CodeWar_1.Accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+        testing(CodeWar_1.Accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+        testing(CodeWar_1.Accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+        testing(CodeWar_1.Accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+    }
+
+    [Test]
+    public static void OneLetter_ReturnsUpperCaseLetter()
+    {
+        var result = CodeWar_1.Accum("a");
+        var expected = "A";
+
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public static void TwoLetter_ReturnsCorrectLetters()
+    {
+        var result = CodeWar_1.Accum("ab");
+        var expected = "A-Bb";
+
+        Assert.AreEqual(expected, result);
+    }
+
+    [Test]
+    public static void ThreeLetter_ReturnsCorrectLetters()
+    {
+        var result = CodeWar_1.Accum("abc");
+        var expected = "A-Bb-Ccc";
+
+        Assert.AreEqual(expected, result);
+    }
+    #endregion
 
 }
