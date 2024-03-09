97b544a34641dc8db21cdf3ebc46510797cf8a76
Saf T-Flaire <Saf T-Flaire@DESKTOP-OV37NBC>
2024-03-09 01:07:58 +0000

    Added The Supermarket Queue

---
+++ CodeWars/CodeWar_6.cs
@@ -0,0 +1,24 @@
+﻿/*
+*                                  CodeWars Number 6
+*                                The Supermarket Queue
+*          https://www.codewars.com/CodeWar_6/57b06f90e298a7b53d000a86/train/csharp
+*/
+
+namespace CodeWars;
+
+public static class CodeWar_6
+{
+    public static long QueueTime(int[] customers, int n)
+    {
+        var tills = new int[n];
+        foreach (var customer in customers)
+        {
+            tills[0] += customer;
+            Array.Sort(tills);
+        }
+
+        Array.Reverse(tills);
+
+        return tills[0];
+    }
+}
