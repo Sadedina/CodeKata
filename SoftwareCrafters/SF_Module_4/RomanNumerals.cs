3b239798caeddf165017c72f49527bcca70f9217
Saf T-Flaire <Saf T-Flaire@DESKTOP-OV37NBC>
2024-03-20 17:20:25 +0000

    Added roman Numerals

--- SoftwareCrafters/SF_Module_4/RomanNumerals.cs
+++ SoftwareCrafters/SF_Module_4/RomanNumerals.cs
@@ -2,53 +2,41 @@
 
 public class RomanNumerals
 {
-    private static readonly Dictionary<int, string> ArabicToRomanDictionary = NumbersDictionary();
+    private static readonly Dictionary<double, string> ArabicToRomanDictionary = NumbersDictionary();
 
     public static string ConvertIntoRomanNumerals(int arabicNumber)
+    {
+        var romanNumeral = "";
+        var intoBaseTenList = IntoBaseTenList(arabicNumber);
+
+        foreach (var numbers in intoBaseTenList)
+        {
+            if (numbers == 0)
+                continue;
+
+            var num = numbers;
+            foreach (var dictionary in ArabicToRomanDictionary)
+            {
+                while (num >= dictionary.Key)
+                {
+                    romanNumeral += dictionary.Value;
+                    num -= dictionary.Key;
+                }
+
+                if (num == 0)
+                    break;
+            }
+        }
+
+        return romanNumeral;
+    }
+
+    private static List<double> IntoBaseTenList(int arabicNumber)
     {
         var numbersIntoList = IntoList(arabicNumber);
         var reverseList = ReverseList(numbersIntoList);
         var convertIntoBaseTens = ConvertToBaseTen(reverseList);
-        var reReveresedList = ReverseList(convertIntoBaseTens);
-
-        if (arabicNumber == 1)
-            return "I";
-
-        if (arabicNumber == 2)
-            return "II";
-
-        if (arabicNumber == 3)
-            return "III";
-
-        if (arabicNumber == 4)
-            return "IV";
-
-        if (arabicNumber == 5)
-            return "V";
-
-        if (arabicNumber == 6)
-            return "VI";
-
-        if (arabicNumber == 7)
-            return "VII";
-
-        if (arabicNumber == 8)
-            return "VIII";
-
-        if (arabicNumber == 9)
-            return "IX";
-
-        if (arabicNumber == 10)
-            return "X";
-
-        return "";
-    }
-
-    private static List<double> ReverseList(List<double> numbers)
-    {
-        numbers.Reverse();
-
-        return numbers;
+        return ReverseList(convertIntoBaseTens);
     }
 
     private static List<double> IntoList(int num)
@@ -60,21 +48,33 @@ public class RomanNumerals
             .ToList();
     }
 
+    private static List<double> ReverseList(List<double> numbers)
+    {
+        numbers.Reverse();
+
+        return numbers;
+    }
+
     private static List<double> ConvertToBaseTen(List<double> numbers)
         => numbers.Select((number, position) => number * Math.Pow(10, position)).ToList();
 
-
-    private static Dictionary<int, string> NumbersDictionary()
+    private static Dictionary<double, string> NumbersDictionary()
     {
         return new()
         {
-            { 1, "I" },
-            { 5, "V" },
-            { 10, "X" },
-            { 50, "L" },
-            { 100, "C" },
+            { 1000, "M" },
+            { 900, "CM" },
             { 500, "D" },
-            { 1000, "M" }
+            { 400, "CD" },
+            { 100, "C" },
+            { 90, "XC" },
+            { 50, "L" },
+            { 40, "XL" },
+            { 10, "X" },
+            { 9, "IX" },
+            { 5, "V" },
+            { 4, "IV" },
+            { 1, "I" }
         };
     }
 }
--- Tests/SoftwareCrafters/SF_Module_4/RomanNumerals_Tests.cs
+++ Tests/SoftwareCrafters/SF_Module_4/RomanNumerals_Tests.cs
@@ -44,4 +44,61 @@ public class RomanNumerals_Tests
         Assert.AreEqual(expectedNumber, result);
     }
 
+    [TestCase(1, "I")]
+    [TestCase(5, "V")]
+    [TestCase(10, "X")]
+    [TestCase(50, "L")]
+    [TestCase(100, "C")]
+    [TestCase(500, "D")]
+    [TestCase(1000, "M")]
+    public void ConvertIntoRomanNumerals_GivenNumbersWithPossibleLetters_ReturnsCorrectRomanNumeral(int arabicNumber, string expectedNumber)
+    {
+        var result = RomanNumerals.ConvertIntoRomanNumerals(arabicNumber);
+
+        Assert.AreEqual(expectedNumber, result);
+    }
+
+    [TestCase(10, "X")]
+    [TestCase(20, "XX")]
+    [TestCase(30, "XXX")]
+    [TestCase(40, "XL")]
+    [TestCase(50, "L")]
+    [TestCase(60, "LX")]
+    [TestCase(70, "LXX")]
+    [TestCase(80, "LXXX")]
+    [TestCase(90, "XC")]
+    public void ConvertIntoRomanNumerals_GivenNumbersMultipleOfTensToNinety_ReturnsCorrectRomanNumeral(int arabicNumber, string expectedNumber)
+    {
+        var result = RomanNumerals.ConvertIntoRomanNumerals(arabicNumber);
+
+        Assert.AreEqual(expectedNumber, result);
+    }
+
+    [TestCase(100, "C")]
+    [TestCase(200, "CC")]
+    [TestCase(300, "CCC")]
+    [TestCase(400, "CD")]
+    [TestCase(500, "D")]
+    [TestCase(600, "DC")]
+    [TestCase(700, "DCC")]
+    [TestCase(800, "DCCC")]
+    [TestCase(900, "CM")]
+    public void ConvertIntoRomanNumerals_GivenNumbersMultipleOfHundredsToNineHundred_ReturnsCorrectRomanNumeral(int arabicNumber, string expectedNumber)
+    {
+        var result = RomanNumerals.ConvertIntoRomanNumerals(arabicNumber);
+
+        Assert.AreEqual(expectedNumber, result);
+    }
+
+    [TestCase(4, "IV")]
+    [TestCase(9, "IX")]
+    [TestCase(29, "XXIX")]
+    [TestCase(294, "CCXCIV")]
+    [TestCase(2023, "MMXXIII")]
+    public void ConvertIntoRomanNumerals_GivenRandomNumbers_ReturnsCorrectRomanNumeral(int arabicNumber, string expectedNumber)
+    {
+        var result = RomanNumerals.ConvertIntoRomanNumerals(arabicNumber);
+
+        Assert.AreEqual(expectedNumber, result);
+    }
 }
