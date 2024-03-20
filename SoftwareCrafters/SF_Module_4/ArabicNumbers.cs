c3600d5e15a0ba3f06e11b7992a58fab7b2dae86
Samuel Adedina <samuel.adedina@asos.com>
2024-03-20 18:08:32 +0000

    Added functionality for Arabic Numbers

--- SoftwareCrafters/SF_Module_4/ArabicNumbers.cs
+++ SoftwareCrafters/SF_Module_4/ArabicNumbers.cs
@@ -2,8 +2,45 @@
 
 public class ArabicNumbers
 {
-    public static int ConvertIntoArabicNumbers(string arabicNumber)
+    private static readonly Dictionary<string, int> RomanToArabicDictionary = NumbersDictionary();
+
+
+    public static int ConvertIntoArabicNumbers(string romanNumeral)
     {
-        return 0;
+        var arabicNumber = 0;
+
+        do
+        {
+            foreach (var item in RomanToArabicDictionary.Where(item => romanNumeral.StartsWith(item.Key)))
+            {
+                arabicNumber += item.Value;
+                romanNumeral = romanNumeral.Remove(0, item.Key.Length);
+                break;
+            }
+
+        } while (romanNumeral.Length != 0);
+
+
+        return arabicNumber;
+    }
+
+    private static Dictionary<string, int> NumbersDictionary()
+    {
+        return new()
+        {
+            { "M", 1000 },
+            { "CM" , 900 },
+            { "D" , 500 },
+            { "CD" , 400 },
+            { "C" , 100 },
+            { "XC" , 90 },
+            { "L" , 50 },
+            { "XL" , 40 },
+            { "X" , 10 },
+            { "IX" , 9 },
+            { "V" , 5 },
+            { "IV" , 4 },
+            { "I" , 1 }
+        };
     }
 }
