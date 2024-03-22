3b2beced53dc2325c56e2fa3554948929ce57c4b
Saf T-Flaire <Saf T-Flaire@DESKTOP-OV37NBC>
2024-03-22 16:44:46 +0000

    Started WordWrap

---
+++ SoftwareCrafters/SF_Module_4/WordWrap.cs
@@ -0,0 +1,17 @@
+﻿namespace SoftwareCrafters.SF_Module_4;
+
+public class WordWrap
+{
+    public static string Wrap(string word, int number)
+    {
+        if (string.IsNullOrEmpty(word))
+            return string.Empty;
+
+        if (word.Length > number)
+            word = word.Insert(number, "/n");
+
+        return word;
+    }
+
+
+}
---
+++ Tests/SoftwareCrafters/SF_Module_4/WordWrap_Tests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using SoftwareCrafters.SF_Module_4;
+
+namespace Tests.SoftwareCrafters.SF_Module_4;
+
+[TestFixture]
+public class WordWrap_Tests
+{
+    [TestCase(null, "")]
+    [TestCase("", "")]
+    public void Wrap_GivenNullOrEmptyWord_ReturnsEmpty(string word, string expectedWord)
+    {
+        var column = 1;
+        var result = WordWrap.Wrap(word, column);
+
+        Assert.AreEqual(expectedWord, result);
+    }
+
+    [TestCase("word", 10, "word")]
+    [TestCase("pizza", 7, "pizza")]
+    [TestCase("tower", 15, "tower")]
+    public void Wrap_GivenAColumNumberIsBiggerThanTheWord_ReturnsWord(string word, int column, string expectedWord)
+    {
+        var result = WordWrap.Wrap(word, column);
+
+        Assert.AreEqual(expectedWord, result);
+    }
+
+    [TestCase("wonderfull", 5, "wonde/nrfull")]
+    [TestCase("spice", 3, "spi/nce")]
+    [TestCase("bullseye", 7, "bullsey/ne")]
+    public void Wrap_GivenAColumNumberIsSmallerThanTheWord_ReturnsWordWithNewLineInbetween(string word, int column, string expectedWord)
+    {
+        var result = WordWrap.Wrap(word, column);
+
+        Assert.AreEqual(expectedWord, result);
+    }
+
+    [TestCase("wonderfull", 4, "wond/nerfu/nll")]
+    [TestCase("restaurant", 3, "res/ntau/nran/nt")]
+    [TestCase("bullseye", 2, "bu/nll/nse/nye")]
+    public void Wrap_GivenAColumNumberIsMoreThanTwiceAsSmallThanTheWord_ReturnsWordWithNewLineInbetween(string word, int column, string expectedWord)
+    {
+        var result = WordWrap.Wrap(word, column);
+
+        Assert.AreEqual(expectedWord, result);
+    }
+
+
+}
