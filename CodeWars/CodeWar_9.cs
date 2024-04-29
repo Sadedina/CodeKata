d76fedacc6437f3b9dfea3e67e67746d20322a0a
Samuel Adedina <samuel.adedina@asos.com>
2024-04-29 13:21:25 +0100

    Created CodeWars 9

---
+++ CodeWars/CodeWar_9.cs
@@ -0,0 +1,10 @@
+﻿/*
+ *                                  CodeWars Number 8
+ *                                 Mount the Bowling Pins!
+ *               https://www.codewars.com/kata/585cf93f6ad5e0d9bf000010/train/csharp
+ */
+namespace CodeWars;
+
+public class CodeWar_9
+{
+}
