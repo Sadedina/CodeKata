3e3c9db85935dd66524802ed86a19fb74d4f542b
Saf T-Flaire <Saf T-Flaire@DESKTOP-OV37NBC>
2024-05-04 12:39:57 +0100

    Added PD

---
+++ PersonalDevelopment.Tests/PD15_Tests.cs
@@ -0,0 +1,71 @@
+namespace PersonalDevelopment.Tests;
+
+public class PD15_Tests
+{
+    [Test]
+    public void CretaeMethodName_GivenInput_ReturnsOutput()
+    {
+        var sample = new List<string>
+        {
+            "GIVENNNNNNNNNNNNNNNNNNNNN",
+            "WHENNNNNNNNNNNNNNNNNNNNN",
+            "THENNNNNNNNNNNNNNNNNNNNN"
+        };
+
+        var givenWhenThen = new List<string>
+        {
+            "GIVEN a client opens a new account",
+            "WHEN the client views their balance",
+            "THEN the client should have an account balance of 0"
+        };
+
+        var methodName = PD15.CretaeMethodName(givenWhenThen);
+
+        Assert.AreNotEqual("", methodName);
+    }
+
+    [TestCaseSource(nameof(TestScript))]
+    public void CretaeMethodName_WhenGivenWehnThenIsValid_ReturnsMethodName(
+        List<string> givenWhenThen,
+        string expectedMethodName)
+    {
+        var methodName = PD15.CretaeMethodName(givenWhenThen);
+
+        Assert.AreEqual(expectedMethodName, methodName);
+    }
+
+    public static IEnumerable<object[]> TestScript()
+    {
+        var expectedNameOne = "GivenAClientOpensANewAccount_WhenTheClientViewsTheirBalance_ThenTheClientShouldHaveAnAccountBalanceOf0";
+        var listNameOne = new List<string>
+        {
+            "GIVEN a client opens a new account",
+            "WHEN the client views their balance",
+            "THEN the client should have an account balance of 0"
+        };
+
+        var expectedNameTwo = "GivenAClientHasAnExistingAccountWithABalanceOf1000_WhenTheClientViewsTheirBalance_ThenTheClientShouldHaveAnAccountBalanceOf1000";
+        var listNameTwo = new List<string>
+        {
+            "GIVEN a client has an existing account with a balance of 1000",
+            "WHEN the client views their balance",
+            "THEN the client should have an account balance of 1000"
+        };
+
+        var expectedNameThree = "GivenAClientOpensANewAccount_WhenTheClientMakesADepositOf500_AndTheClientMakesADepositOf200_ThenTheClientShouldHaveANewAccountBalanceOf700";
+        var listNameThree = new List<string>
+        {
+            "GIVEN a client opens a new account",
+            "WHEN the client makes a deposit of 500",
+            "AND the client makes a deposit of 200",
+            "THEN the client should have a new account balance of 700"
+        };
+
+        return new List<object[]>
+        {
+            new object[] { listNameOne, expectedNameOne },
+            new object[] { listNameTwo, expectedNameTwo },
+            new object[] { listNameThree, expectedNameThree }
+        };
+    }
+}
---
+++ PersonalDevelopment/PD15.cs
@@ -0,0 +1,51 @@
+﻿namespace PersonalDevelopment;
+
+public class PD15
+{
+    public static string CretaeMethodName(List<string> method)
+    {
+        var methodName = "";
+
+        for (int i = 0; i < method.Count; i++)
+        {
+            methodName += SanitiseSentence(method[i]);
+
+            if (i < method.Count - 1)
+                methodName += "_";
+        }
+
+        return methodName;
+    }
+
+    private static string SanitiseSentence(string sentence)
+    {
+        var finalSentence = "";
+
+        var words = sentence.Split(' ');
+
+        foreach (var word in words)
+        {
+            finalSentence += SanitiseWord(word);
+        }
+
+        return finalSentence;
+    }
+
+    private static string SanitiseWord(string word)
+    {
+        var finalWord = "";
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            var wordChar = word[i].ToString().ToLower();
+
+            if (i == 0)
+                wordChar = wordChar.ToUpper();
+
+            finalWord += wordChar;
+        }
+
+        return finalWord;
+    }
+
+}
