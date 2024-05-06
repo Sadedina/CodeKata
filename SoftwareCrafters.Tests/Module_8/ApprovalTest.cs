4d8cfc5790e1244c05794a6f064c7e866336a613
Saf T-Flaire <Saf T-Flaire@DESKTOP-OV37NBC>
2024-05-06 17:25:01 +0100

    Added module8

---
+++ SoftwareCrafters.Tests/Module_8/ApprovalTest.cs
@@ -0,0 +1,62 @@
+﻿using System.Text;
+using SoftwareCrafters.Module_8;
+
+#region Original
+//namespace SoftwareCrafters.Tests.Module_8
+//{
+//    [TestFixture]
+//    public class ApprovalTest
+//    {
+//        [Test]
+//        public void ThirtyDays()
+//        {
+//            var filePath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "ThirtyDays.txt");
+
+//            var referenceLines = File.ReadAllLines(filePath);
+
+//            var fakeoutput = new StringBuilder();
+//            Console.SetOut(new StringWriter(fakeoutput));
+//            Console.SetIn(new StringReader("\n"));
+
+//            Program.Main(new string[] { });
+//            var output = fakeoutput.ToString();
+
+//            var outputLines = output.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+//            for (var i = 0; i < Math.Min(referenceLines.Length, outputLines.Length); i++)
+//            {
+//                Assert.That(referenceLines[i], Is.EqualTo(outputLines[i]));
+//            }
+//        }
+//    }
+//}
+#endregion
+
+namespace SoftwareCrafters.Tests.Module_8;
+
+[TestFixture]
+public class ApprovalTest
+{
+    [Test]
+    public void ThirtyDays()
+    {
+        var directory = Environment.CurrentDirectory.Split("bin").First();
+        var filePath = Path.Combine(directory, "Module_8", "ThirtyDays.txt");
+
+        var referenceLines = File.ReadAllLines(filePath);
+
+        var fakeoutput = new StringBuilder();
+        Console.SetOut(new StringWriter(fakeoutput));
+        Console.SetIn(new StringReader("\n"));
+
+        Program.Main(new string[] { });
+        var output = fakeoutput.ToString();
+
+        var outputLines = output.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+
+        for (var i = 0; i < Math.Min(referenceLines.Length, outputLines.Length); i++)
+        {
+            Assert.That(referenceLines[i], Is.EqualTo(outputLines[i]));
+        }
+    }
+}
---
+++ SoftwareCrafters.Tests/Module_8/GildedRoseTest.cs
@@ -0,0 +1,34 @@
+﻿using SoftwareCrafters.Module_8;
+
+#region Original
+//namespace SoftwareCrafters.Tests.Module_8
+//{
+//    [TestFixture]
+//    public class GildedRoseTest
+//    {
+//        [Test]
+//        public void foo()
+//        {
+//            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+//            GildedRose app = new GildedRose(Items);
+//            app.UpdateQuality();
+//            Assert.That("fixme", Is.EqualTo(Items[0].Name));
+//        }
+//    }
+//}
+#endregion
+
+namespace SoftwareCrafters.Tests.Module_8;
+
+[TestFixture]
+public class GildedRoseTest
+{
+    [Test]
+    public void foo()
+    {
+        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        GildedRose app = new GildedRose(Items);
+        app.UpdateQuality();
+        Assert.That("fixme", Is.EqualTo(Items[0].Name));
+    }
+}
