1bc5f22c9230f5dfac301c29f081acede21e0d84
Samuel Adedina <samuel.adedina@asos.com>
2024-05-14 16:55:26 +0100

    refactor test

--- PersonalDevelopment.Tests/PD15_Tests.cs
+++ PersonalDevelopment.Tests/PD15_Tests.cs
@@ -3,9 +3,9 @@ namespace PersonalDevelopment.Tests;
 public class PD15_Tests
 {
     [Test]
-    public void CretaeMethodName_GivenInput_ReturnsOutput()
+    public void CreateMethodName_GivenInput_ReturnsOutput()
     {
-        var givenWhenThen = "GIVEN a client opens a new account\r\nWHEN the client makes a withdrawal of 1001\r\nTHEN the withdrawal should be rejected\r\nAND the client should be told they can not exceed overdraft of 1000";
+        const string givenWhenThen = "GIVEN a client opens a new account\r\nWHEN the client makes a withdrawal of 1001\r\nTHEN the withdrawal should be rejected\r\nAND the client should be told they can not exceed overdraft of 1000";
 
         var methodName = PD15.CreateMethodName(givenWhenThen);
 
@@ -13,7 +13,7 @@ public class PD15_Tests
     }
 
     [TestCaseSource(nameof(TestScript))]
-    public void CretaeMethodName_WhenGivenWehnThenIsValid_ReturnsMethodName(
+    public void CreateMethodName_WhenGivenWhenThenIsValid_ReturnsMethodName(
         List<string> givenWhenThen,
         string expectedMethodName)
     {
@@ -24,7 +24,7 @@ public class PD15_Tests
 
     public static IEnumerable<object[]> TestScript()
     {
-        var expectedNameOne = "GivenAClientOpensANewAccount_WhenTheClientViewsTheirBalance_ThenTheClientShouldHaveAnAccountBalanceOf0";
+        const string expectedNameOne = "GivenAClientOpensANewAccount_WhenTheClientViewsTheirBalance_ThenTheClientShouldHaveAnAccountBalanceOf0";
         var listNameOne = new List<string>
         {
             "GIVEN a client opens a new account",
@@ -32,7 +32,7 @@ public class PD15_Tests
             "THEN the client should have an account balance of 0"
         };
 
-        var expectedNameTwo = "GivenAClientHasAnExistingAccountWithABalanceOf1000_WhenTheClientViewsTheirBalance_ThenTheClientShouldHaveAnAccountBalanceOf1000";
+        const string expectedNameTwo = "GivenAClientHasAnExistingAccountWithABalanceOf1000_WhenTheClientViewsTheirBalance_ThenTheClientShouldHaveAnAccountBalanceOf1000";
         var listNameTwo = new List<string>
         {
             "GIVEN a client has an existing account with a balance of 1000",
@@ -40,7 +40,7 @@ public class PD15_Tests
             "THEN the client should have an account balance of 1000"
         };
 
-        var expectedNameThree = "GivenAClientOpensANewAccount_WhenTheClientMakesADepositOf500_AndTheClientMakesADepositOf200_ThenTheClientShouldHaveANewAccountBalanceOf700";
+        const string expectedNameThree = "GivenAClientOpensANewAccount_WhenTheClientMakesADepositOf500_AndTheClientMakesADepositOf200_ThenTheClientShouldHaveANewAccountBalanceOf700";
         var listNameThree = new List<string>
         {
             "GIVEN a client opens a new account",
